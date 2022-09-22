1971b3c7b2c9025fef6a136cffefe2711b091f99
liuyuanhua <1764962071@qq.com>
2022-09-22 10:47:58 +0800

    build: 对自动导入进行了修改

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,9 @@ import store from "./store"
 import ElementPlus from "element-plus"
 import "element-plus/dist/index.css"
 import zhCn from "element-plus/es/locale/lang/zh-cn"
-import * as ElIcons from "@element-plus/icons-vue"
-import * as AntIcons from "@ant-design/icons-vue"
-import Ant from "@/plugins/ant-design"
-// import "ant-design-vue/dist/antd.css" // or 'ant-design-vue/dist/antd.less'
-// import "ant-design-vue/lib/menu/style/css" // or 'ant-design-vue/dist/antd.less'
+// import * as ElIcons from "@element-plus/icons-vue"
+// import * as AntIcons from "@ant-design/icons-vue"
+// import Ant from "@/plugins/ant-design"
 const el = createApp(App)
 
 el.use(store)
@@ -18,14 +16,15 @@ el.use(ElementPlus, {
   locale: zhCn,
 })
 
-for (const [key, component] of Object.entries(ElIcons)) {
-  el.component(key, component)
-}
-for (const [key, component] of Object.entries(AntIcons)) {
-  el.component(key, component)
-}
-for (const [key, component] of Object.entries(Ant)) {
-  el.component(`A${key}`, component) // 因为ant 都是以a开头所以拼接上a
-}
+// 此方法可导入全部组件 若在vue.config 文件中没有使用自动导入组件 则可使用此方法
+// for (const [key, component] of Object.entries(ElIcons)) {
+//   el.component(key, component)
+// }
+// for (const [key, component] of Object.entries(AntIcons)) {
+//   el.component(key, component)
+// }
+// for (const [key, component] of Object.entries(Ant)) {
+//   el.component(`A${key}`, component) // 因为ant 都是以a开头所以拼接上a
+// }
 
 el.mount("#app")
