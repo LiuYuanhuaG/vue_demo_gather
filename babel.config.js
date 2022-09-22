3c99044b6e80ab3227c1633eff6d996ea2b9e146
liuyuanhua <1764962071@qq.com>
2022-09-22 10:39:00 +0800

    build: 对自动导入进行了修改

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
-const AutoImport = require("unplugin-auto-import/webpack")
-const Components = require("unplugin-vue-components/webpack")
-const { ElementPlusResolver, AntDesignVueResolver } = require("unplugin-vue-components/resolvers") // 添加ant 与element puls的 自动导入
-
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
   plugins: [
